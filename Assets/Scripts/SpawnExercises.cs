c3fbddd82f1117acf84e5a28a7b7de6a1b0e69f0
MarvMa <marvin.meitzner@gmail.com>
2022-06-23 18:28:41 +0200

    changed style

--- Assets/Scripts/SpawnExercises.cs
+++ Assets/Scripts/SpawnExercises.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class SpawnExercises : MonoBehaviour
 {
+    // Public Variables
     public GameObject podestGameObject;
+    public GameObject spotLight;
 
-    public Vector3[] podestPositions = new[]
+    private Vector3[] podestPositions = new[]
     {
         new Vector3(10, 0.1f, 10),
         new Vector3(14, 0.1f, 0),
@@ -18,7 +20,7 @@ public class SpawnExercises : MonoBehaviour
 
     void Start()
     {
-        GameObject[] podests= spawnPodests();
+        GameObject[] podests = spawnPodests();
         Debug.Log(podests);
     }
 
@@ -33,7 +35,9 @@ public class SpawnExercises : MonoBehaviour
         int index = 0;
         foreach (var position in podestPositions)
         {
-            podests[index] =  Instantiate(podestGameObject, position, Quaternion.identity);
+            podests[index] = Instantiate(podestGameObject, position, Quaternion.identity);
+            GameObject light = Instantiate(spotLight, new Vector3(position.x, 5, position.z), Quaternion.identity);
+            light.transform.Rotate(90, 0, 0);
             index++;
         }
 
