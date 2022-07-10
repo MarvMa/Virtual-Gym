f957d22fbd0cc2e24dcd28d98d930811d72987e8
Ostervogel <m.lozahenrich@gmail.com>
2022-07-10 15:25:58 +0200

    Trainingsplan logic

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -31,6 +31,7 @@ public class MenuManager : MonoBehaviour
     private List<TrainingPlan> filteredTrainingsPlan;
     private List<InnerExercise> availableExercises;
     private List<String> exercisesAsStrings;
+    private List<InnerExercise> helper;
 
 
 
@@ -270,11 +271,14 @@ public class MenuManager : MonoBehaviour
                 //0-1-2
                 availableExercises = filteredTrainingsPlan[trainingsPlan - 1].exercises.OrderBy(o => o.exerciseDifficulty).ToList();
                 break;
-                //1-0-2
+            //1-0-2 kind of unnessesary because there will always be 9 elements with difficulty 1 and 0 and 2 will never be included
             case 1:
-                availableExercises = availableExercises.OrderBy(i => i.exerciseDifficulty == 2).ThenByDescending(i => i).ToList();
+                availableExercises = filteredTrainingsPlan[trainingsPlan - 1].exercises.OrderByDescending(o => o.exerciseDifficulty).ToList();
+                helper = availableExercises.Where(o => o.exerciseDifficulty == 2).ToList();
+                availableExercises = availableExercises.Except(helper).ToList();
+                availableExercises.AddRange(helper);
                 break;
-                //2-1-0
+            //2-1-0
             case 2:
                 availableExercises = filteredTrainingsPlan[trainingsPlan - 1].exercises.OrderByDescending(o => o.exerciseDifficulty).ToList();
                 break;
@@ -284,32 +288,31 @@ public class MenuManager : MonoBehaviour
         switch (trainingsDuration)
         {
             case 0:
-                availableExercises.Take(3);
+                availableExercises = availableExercises.Take(3).ToList();
                 break;
 
             case 1:
-                availableExercises.Take(6);
+                availableExercises = availableExercises.Take(6).ToList();
                 break;
 
             case 2:
-                availableExercises.Take(9);
+                availableExercises = availableExercises.Take(9).ToList();
                 break;
-    
+
         }
 
         exercisesAsStrings = new List<string>();
         //Return only the string names of the exercises in a list
         foreach (var item in availableExercises)
         {
-            
+
             exercisesAsStrings.Add(item.exerciseName);
         }
-        Debug.Log(exercisesAsStrings.ToString());
         return exercisesAsStrings;
 
     }
     // async 
-        void spawnScene()
+    void spawnScene()
     {
 
         // await Task.Delay(2500);
